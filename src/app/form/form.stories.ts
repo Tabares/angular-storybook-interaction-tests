572c1544612c8a14138a27f0e2d4758efb448ded
Jose Tabares Sotelo <jose.sotelo@cat.com>
2023-04-20 19:11:00 -0600

    Add assertions

--- src/app/form/form.stories.ts
+++ src/app/form/form.stories.ts
@@ -2,6 +2,7 @@ import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { FormComponent } from './form.component';
 import { userEvent, within } from '@storybook/testing-library';
 import { ReactiveFormsModule } from '@angular/forms';
+import { expect } from '@storybook/jest';
 
 const meta: Meta<FormComponent> = {
   title: 'Example/Form',
@@ -39,15 +40,22 @@ export const EmptyForm: Story = {};
 export const Submitted: Story = {
   play: async ({ args, canvasElement, step }) => {
     const canvas = within(canvasElement);
+    const submitButton = canvas.getByTestId('submit') as any;
 
     await step('Enter user and address information', async () => {
+      await expect(submitButton).toHaveProperty('disabled');
+
       await userEvent.click(canvas.getByTestId('first-name'));
       await userEvent.type(canvas.getByTestId('first-name'), 'John');
+
       await userEvent.type(canvas.getByTestId('last-name'), 'Wick');
       await userEvent.type(canvas.getByTestId('street'), '121 Mill Neck');
       await userEvent.type(canvas.getByTestId('city'), 'Long Island');
       await userEvent.type(canvas.getByTestId('state'), 'NY');
       await userEvent.type(canvas.getByTestId('zip'), '11765');
+
+      await expect(submitButton).toHaveProperty('disabled');
+      await expect(submitButton).toHaveProperty('type', 'submit');
     });
 
     await step('Enter two alias', async () => {
@@ -60,12 +68,12 @@ export const Submitted: Story = {
     });
 
     await step('Submit form', async () => {
-      await userEvent.click(canvas.getByTestId('submit'));
+      await userEvent.click(submitButton);
     });
 
     await step('Auto populate and submit', async () => {
       await userEvent.click(canvas.getByTestId('auto-populate'));
-      await userEvent.click(canvas.getByTestId('submit'));
+      await userEvent.click(submitButton);
     });
   },
   args: {
